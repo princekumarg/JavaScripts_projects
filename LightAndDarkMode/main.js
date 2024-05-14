7d42d9e3884c72e08091631bf4d261b7185e6044
prince kumar <agarwal8789@gmail.com>
2024-05-14 18:08:15 +0530

    commit made

---
+++ LightAndDarkMode/main.js
@@ -0,0 +1,19 @@
+const changeThemeBtn = document.querySelector(".change-theme-btn");
+const body = document.querySelector("body");
+
+
+changeThemeBtn.addEventListener("click", () => {
+  console.log(body.getAttribute("data-theme"));
+  if (body.getAttribute("data-theme") === "dark") {
+    body.setAttribute("data-theme", 'blue');
+  } else {
+    body.setAttribute("data-theme", "dark");
+  }
+
+  if (changeThemeBtn.getAttribute("data-theme") === "dark") {
+    changeThemeBtn.setAttribute("data-theme", 'blue');
+  } else {
+    changeThemeBtn.setAttribute("data-theme", "dark");
+  }
+//   changeThemeBtn.setAttribute("data-theme", "dark");
+});
