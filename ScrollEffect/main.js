43068eee355c15bbfe3034c1ffd19efa49515ca7
prince kumar <agarwal8789@gmail.com>
2024-05-18 20:06:20 +0530

    commit made on scrollEffect

---
+++ ScrollEffect/main.js
@@ -0,0 +1,46 @@
+const usersList=document.querySelector('.users-list');
+const loader=document.querySelector('.loader');
+const scrollToTopButton=document.querySelector('.scroll-to-top-button');
+const scrollToBottomButton=document.querySelector('.scroll-to-bottom-button');
+
+function showLoader(){
+    loader.classList.add("show-loader");
+    usersList.classList.add("hide-users-list");
+}
+
+function removeLoader(){
+    loader.classList.remove("show-loader");
+    usersList.classList.remove("hide-users-list");
+}
+
+async function fetchUsersList(){
+    showLoader();
+    const response=await fetch('https://dummyjson.com/users?limit=100',{
+        method:'GET',
+    
+    });
+    const result=await response.json();
+    if(result && result.users) displayUsersList(result.users);
+    removeLoader();
+}
+function displayUsersList(getUsers){
+    usersList.innerHTML=getUsers.map((userItem)=>`
+    <li>
+    <p>${userItem.firstName} ${userItem.lastName}</p>
+    </li>`).join(" ");
+}
+fetchUsersList();
+scrollToTopButton.addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        behavior:'smooth',
+    });
+});
+scrollToBottomButton.addEventListener('click',()=>{
+    window.scrollTo({
+        top:document.body.scrollHeight,
+        behavior:'smooth',
+    });
+});
+
+    
